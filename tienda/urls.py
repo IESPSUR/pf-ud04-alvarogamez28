0a92c55f6dae4543e70664f410ff854f65d726fc
Tu Nombre <you@example.com>
2022-11-15 09:48:57 +0100

    cambios

--- tienda/urls.py
+++ tienda/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('listadoProductos/', views.listadoProductos, name='listadoProductos'),
     path('tienda/', views.welcome, name='welcome'),
+    path('producto/nuevo/', views.nuevoProducto, name='nuevoProducto'),
+    path('producto/<str:pk>',views.eliminarProducto, name='eliminarProducto'),
+    path('producto/editar/<str:pk>', views.editarProducto, name='editarProducto'),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -1,9 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Producto
+from .form import PostForm
 
 # Create your views here.
 def welcome(request):
     return render(request,'tienda/index.html', {})
 
-def listadoProductos(request):
 
-    return render(request,'tienda/listadoProductos.html', {})
+
+def listadoProductos(request):
+    productos = Producto.objects.filter().order_by('nombreProducto')
+    return render(request,'tienda/listadoProductos.html', {'productos': productos})
+
+
+def nuevoProducto(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            producto = form.save(commit=False)
+            producto.save()
+            productos = Producto.objects.filter().order_by('nombreProducto')
+            return render(request, 'tienda/listadoProductos.html', {'productos': productos})
+    else:
+        form = PostForm()
+    return render(request, 'tienda/producto_nuevo.html', {'form': form})
+
+def eliminarProducto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    producto.delete()
+    productos = Producto.objects.filter().order_by('nombreProducto')
+    return render(request, 'tienda/listadoProductos.html', {'productos': productos})
+
+def editarProducto(request, pk):
+    post = get_object_or_404(Producto, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            producto = form.save(commit=False)
+            producto.save()
+            productos = Producto.objects.filter().order_by('nombreProducto')
+            return render(request, 'tienda/listadoProductos.html', {'productos': productos})
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/productoNuevo.html', {'form': form})
